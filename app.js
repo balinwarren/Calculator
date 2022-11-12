1cd0cad7b7fbdbeb9aeae1af1e1e5a02382bcafa
Balin Warren <balin.warren@gmail.com>
2022-11-12 13:40:39 -0800

    Added decimal point button fucntionality

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const display = document.getElementById("display-text");
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.getElementById("equals-btn");
-const decimalButton = document.getElementById("points-btn");
+const decimalButton = document.getElementById("point-btn");
 const clearButton = document.getElementById("clear-btn");
 const deleteButton = document.getElementById("delete-btn");
 
 clearButton.addEventListener('click', clear);
-deleteButton.addEventListener('click', deleteNum)
+deleteButton.addEventListener('click', deleteNum);
+decimalButton.addEventListener('click', appendPoint);
 
 numberButtons.forEach((button) =>
   button.addEventListener('click', () => appendNum(button.textContent))
@@ -36,6 +37,14 @@ function appendNum(num) {
     refreshScreen()
   }
   display.textContent += num;
+}
+
+function appendPoint() {
+  if (resetScreen) refreshScreen()
+  if (display.textContent === '')
+    display.textContent = '0'
+  if (display.textContent.includes('.')) return
+  display.textContent += '.'
 }
 
 function deleteNum() {
